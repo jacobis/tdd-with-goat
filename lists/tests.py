3e5996108322cd7a48ffbaefbfdc56c0c72db4f9
jacobis <remind.jacob@gmail.com>
2017-02-06 12:09:08 +0900

    Only one task can add to task list

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,4 +15,17 @@ class SmokeTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'home.html', {'new_item_text': '신규 작업 아이템'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
